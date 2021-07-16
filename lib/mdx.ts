3e3a70e5415008b1d8dbdfc714e4ee1fffd01a11
Mats Larsen <me@supergrecko.com>
2021-07-16 23:29:38 +0200

    fix: use promises destructure import for fs.promises
    Vercel does not run on a node version that has the fs/promises module

--- lib/mdx.ts
+++ lib/mdx.ts
@@ -1,4 +1,4 @@
-import fs from 'fs/promises'
+import { promises as fs } from 'fs'
 import { serialize } from 'next-mdx-remote/serialize'
 import { MDXRemoteSerializeResult } from 'next-mdx-remote'
 import path from 'path'
