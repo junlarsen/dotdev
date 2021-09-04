9a6f05fc08550e1834a4e94a3e10023abf64bfbd
Mats Larsen <me@supergrecko.com>
2021-09-04 22:00:45 +0200

    terraform: allow cors through api gateway

--- terraform/api-gateway.tf
+++ terraform/api-gateway.tf
@@ -2,25 +2,19 @@ resource "aws_apigatewayv2_api" "dotdev-api" {
   name = "dotdev-api"
   description = "Main dotdev backend API"
   protocol_type = "HTTP"
+  cors_configuration {
+    allow_origins = [
+      "https://supergrecko.com",
+      "https://supergrecko.dev",
+      "https://matsjla.no"
+    ]
+  }
 }
 
 resource "aws_apigatewayv2_stage" "aws-lambda" {
   api_id = aws_apigatewayv2_api.dotdev-api.id
   name = "$default"
   auto_deploy = true
-}
-
-resource "aws_apigatewayv2_integration" "github-activity-stream-integration" {
-  api_id = aws_apigatewayv2_api.dotdev-api.id
-  integration_type = "AWS_PROXY"
-  integration_method = "POST"
-  integration_uri = aws_lambda_function.github_activity_stream.invoke_arn
-}
-
-resource "aws_apigatewayv2_route" "get-github-activity-stream" {
-  api_id = aws_apigatewayv2_api.dotdev-api.id
-  route_key = "GET /v1/github-activity"
-  target = "integrations/${aws_apigatewayv2_integration.github-activity-stream-integration.id}"
 }
 
 resource "aws_apigatewayv2_domain_name" "api-supergrecko-dev" {
@@ -33,7 +27,21 @@ resource "aws_apigatewayv2_domain_name" "api-supergrecko-dev" {
 }
 
 resource "aws_apigatewayv2_api_mapping" "get-github-activity-stream" {
-  api_id      = aws_apigatewayv2_api.dotdev-api.id
+  api_id = aws_apigatewayv2_api.dotdev-api.id
   domain_name = aws_apigatewayv2_domain_name.api-supergrecko-dev.id
-  stage       = aws_apigatewayv2_stage.aws-lambda.id
+  stage = aws_apigatewayv2_stage.aws-lambda.id
+}
+
+# Integration for /v1/github-activity
+resource "aws_apigatewayv2_integration" "github-activity-stream-integration" {
+  api_id = aws_apigatewayv2_api.dotdev-api.id
+  integration_type = "AWS_PROXY"
+  integration_method = "POST"
+  integration_uri = aws_lambda_function.github_activity_stream.invoke_arn
+}
+
+resource "aws_apigatewayv2_route" "get-github-activity-stream" {
+  api_id = aws_apigatewayv2_api.dotdev-api.id
+  route_key = "GET /v1/github-activity"
+  target = "integrations/${aws_apigatewayv2_integration.github-activity-stream-integration.id}"
 }
